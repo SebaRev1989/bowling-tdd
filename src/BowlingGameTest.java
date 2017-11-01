e4cecfea3e16412ac51c3fbeb578e8c6f6b7a0dc
Sebastian Reverso <sebastian.reverso@sovos.com>
2017-11-01 16:39:51 -0300

    Second test with duplicated code

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -7,4 +7,11 @@ public class BowlingGameTest extends TestCase {
             g.roll(0);
         assertEquals(0, g.score());
     }
+
+    public void testAllOnes() throws Exception {
+        Game g = new Game();
+        for (int i=0; i<20; i++)
+            g.roll(1);
+        assertEquals(20, g.score());
+    }
 }
