1e0d2db31ab2aa6a8356320073d0484dfca677e9
Sebastian Reverso <sebastian.reverso@sovos.com>
2017-11-01 16:53:51 -0300

    Second test without duplicated code

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,17 +1,24 @@
 import junit.framework.TestCase;
 
 public class BowlingGameTest extends TestCase {
-    public void testGutterGame() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(0);
-        assertEquals(0, g.score());
-    }
+  private Game g;
 
-    public void testAllOnes() throws Exception {
-        Game g = new Game();
-        for (int i=0; i<20; i++)
-            g.roll(1);
-        assertEquals(20, g.score());
-    }
+  protected void setUp() throws Exception {
+    g = new Game();
+  }
+
+  private void rollMany(int n, int pins) {
+    for (int i=0; i<20; i++)
+      g.roll(pins);
+  }
+
+  public void testGutterGame() throws Exception {
+    rollMany(20, 0);
+    assertEquals(0, g.score());
+  }
+
+  public void testAllOnes() throws Exception {
+    rollMany(20,1);
+    assertEquals(20, g.score());
+  }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,11 @@
 public class Game {
-    private int score = 0;
+  private int score = 0;
 
-    public void roll(int pins) {
+  public void roll(int pins) {
         score += pins;
     }
 
-    public int score() {
-        return score;
-    }
+  public int score() {
+    return score;
+  }
 }
