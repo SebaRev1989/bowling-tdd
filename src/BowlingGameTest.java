bc75021bb95c2f334b0888496269ebd1fdc3983d
Sebastian Reverso <sebastian.reverso@sovos.com>
2017-11-02 15:09:38 -0300

    Fourth test and Strike check

--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -29,6 +29,18 @@ public class BowlingGameTest extends TestCase {
     assertEquals(16,g.score());
   }
 
+  public void testOneStrike() throws Exception {
+    rollStrike();
+    g.roll(3);
+    g.roll(4);
+    rollMany(16,0);
+    assertEquals(24,g.score());
+  }
+
+  private void rollStrike() {
+    g.roll(10);
+  }
+
   private void rollSpare() {
     g.roll(5);
     g.roll(5);
--- src/Game.java
+++ src/Game.java
@@ -10,13 +10,34 @@ public class Game {
     int score = 0;
     int frameIndex = 0;
     for (int frame = 0; frame < 10; frame++) {
-      if (isSpare(frameIndex)) //spare
-        score += 10 + rolls[frameIndex + 2];
-      else
-        score += rolls[frameIndex] + rolls[frameIndex + 1];
-      frameIndex += 2;
+      if (isStike(frameIndex)) {
+        score += 10 + strikeBonus(frameIndex);
+        frameIndex++;
+      } else {
+        if (isSpare(frameIndex))
+          score += 10 + spareBonus(frameIndex);
+        else
+          score += sumOfBallsInFrame(frameIndex);
+        frameIndex += 2;
+      }
     }
     return score;
+  }
+
+  private int sumOfBallsInFrame(int frameIndex) {
+    return (rolls[frameIndex] + rolls[frameIndex + 1]);
+  }
+
+  private int spareBonus(int frameIndex) {
+    return rolls[frameIndex + 2];
+  }
+
+  private int strikeBonus(int frameIndex) {
+    return (rolls[frameIndex + 1] + rolls [frameIndex + 2]);
+  }
+
+  private boolean isStike(int frameIndex) {
+    return rolls[frameIndex] == 10;
   }
 
   private boolean isSpare(int frameIndex) {
